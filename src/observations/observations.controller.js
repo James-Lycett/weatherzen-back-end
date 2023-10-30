57fc56eea95a87db4488e49b43ae08610452cfbd
James-Lycett <jrlycett@mail.com>
2023-10-29 19:05:13 -0700

    connected db and configured .env, added observations.service for db calls

--- src/observations/observations.controller.js
+++ src/observations/observations.controller.js
@@ -1,5 +1,6 @@
-let nextId = 1
-const obervations = []
+const service = require("./observations.service")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+
 
 const validSkyConditions = [100, 101, 102, 103, 104, 106, 108, 109]
 
@@ -47,14 +48,7 @@ function hasSkyCondition(req, res, next) {
 }
 
 async function create(req, res) {
-    const newObservation = req.body.data
-
-    const now = new Date().toISOString()
-    newObservation.observation_id = nextId++
-    newObservation.created_at = now
-    newObservation.updated_at = now
-
-    observations.push(newObservation)
+    const newObservation = await service.create(req.body.data)
 
     res.status(201).json({
         data: newObservation,
@@ -62,12 +56,13 @@ async function create(req, res) {
 }
 
 async function list(req, res) {
+    const data = await service.list()
     res.json({
-        data: observations,
+        data,
     })
 }
 
 module.exports = {
-    create: [hasData, hasLatitude, hasLongitude, hasSkyCondition, create],
-    list
+    create: [hasData, hasLatitude, hasLongitude, hasSkyCondition, asyncErrorBoundary(create)],
+    list: asyncErrorBoundary(list)
 }
---
+++ src/observations/observations.service.js
@@ -0,0 +1,17 @@
+const knex = require("../db/connection")
+
+function create(newObservation) {
+    return knex("observations")
+        .insert(newObservation)
+        .returning("*");
+}
+
+async function list() {
+    return knex("observations")
+    .select("*");
+}
+
+module.exports = {
+    create,
+    list,
+}
