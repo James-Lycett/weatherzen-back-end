32334e59606b2cb30621301f8e38c1989441c864
James-Lycett <jrlycett@mail.com>
2023-11-02 08:44:31 -0700

    added 'air_temperature' and 'air_temperature_unit' columns to observations db, added validation functions for them in observations.controller

---
+++ src/db/migrations/20231102075638_addTempColumns.js
@@ -0,0 +1,14 @@
+// adding temperature columns, max and min validation to be done at business level
+exports.up = function(knex) {
+  return knex.schema.alterTable("observations", (table) => {
+    table.integer("air_temperature", null);
+    table.string("air_temperature_unit", null);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("observations", (table) => {
+    table.dropColumn("air_temperature");
+    table.dropColumn("air_temperature_unit");
+  })
+};
--- src/observations/observations.controller.js
+++ src/observations/observations.controller.js
@@ -1,52 +1,91 @@
 const service = require("./observations.service")
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
 
+// functions to handle CRUDL requests
 
 const validSkyConditions = [100, 101, 102, 103, 104, 106, 108, 109]
 
+// request body validation
 function hasData(req, res, next) {
     if (req.body.data) {
         return next()
+    } else {
+        next({
+            status: 400,
+            message:"body must have data property"
+        })
     }
-    next({
-        status: 400,
-        message:"body must have data property"
-    })
 }
 
+// latitude request property validation
 function hasLatitude(req, res, next) {
     const latitude = Number(req.body.data.latitude)
     if (latitude >= -90 && latitude <= 90) {
         return next()
+    } else {
+        next({
+            status: 400,
+            message: "latitude must be between -90 and 90"
+        })
     }
-    next({
-        status: 400,
-        message: "latitude must be between -90 and 90"
-    })
 }
 
+// longitude request property validation
 function hasLongitude(req, res, next) {
     const longitude = Number(req.body.data.longitude)
     if (longitude >= -180 && longitude <= 180) {
         return next()
+    } else {
+        next({
+            status: 400, 
+            message: "longitude must be between -180 and 180"
+        })
     }
-    next({
-        status: 400, 
-        message: "longitude must be between -180 and 180"
-    })
 }
 
+// sky_condition request property validation
 function hasSkyCondition(req, res, next) {
     const skyCondition = Number(req.body.data.sky_condition)
     if (validSkyConditions.includes(skyCondition)) {
         return next()
+    } else {
+        next({
+            status: 400,
+            message: `sky_condition must be one of: ${validSkyConditions}`
+        })
     }
-    next({
-        status: 400,
-        message: `sky_condition must be one of: ${validSkyConditions}`
-    })
 }
 
+// air_temperature_unit request property validation, should run BEFORE 'hasAirTemperature' which also references this data
+function hasAirTemperatureUnit(req, res, next) {
+    const airTemperatureUnit = req.body.data.air_temperature_unit
+    if (airTemperatureUnit.toUpperCase() === "C" || airTemperatureUnit.toUpperCase() === "F" ) {
+        return next()
+    } else {
+        return next({
+            status: 400,
+            message: `air_temperature_unit must be either C or F`
+        })
+    }
+}
+
+// air_temperature request property validation
+function hasAirTemperature(req, res, next) {
+    const airTemperature = Number(req.body.data.air_temperature)
+    const airTemperatureUnit = req.body.data.air_temperature_unit
+    if (airTemperatureUnit.toUpperCase() === "C" && airTemperature > -50 && airTemperature < 107) {
+        return next()
+    } else if (airTemperatureUnit.toUpperCase() === "F" && airTemperature > -60 && airTemperature < 224) {
+        return next()
+    } else {
+        next({
+            status: 400,
+            message: `air_temperature must be between -50 and 107 degrees C or -60 and 224 degrees F`
+        })
+    }
+}
+
+// POST request handler
 async function create(req, res) {
     const newObservation = await service.create(req.body.data)
 
@@ -55,6 +94,7 @@ async function create(req, res) {
     })
 }
 
+// GET all request handler
 async function list(req, res) {
     const data = await service.list()
     res.json({
@@ -62,12 +102,14 @@ async function list(req, res) {
     })
 }
 
+// GET by observationId request handler
 async function read(req, res) {
     const { observationId } = req.params
     const data = await service.read(observationId)
     res.json({ data })
 }
 
+// check database for observation with given id
 async function observationExists(req, res, next) {
     const { observationId } = req.params
   
@@ -77,8 +119,9 @@ async function observationExists(req, res, next) {
       return next();
     }
     return next({ status: 404, message: `Observation cannot be found.` });
-  }
+}
 
+// PUT request handler
 async function update(req, res) {     
     const updatedObservation = {
         ...req.body.data,
@@ -89,8 +132,22 @@ async function update(req, res) {
 }
 
 module.exports = {
-    create: [hasData, hasLatitude, hasLongitude, hasSkyCondition, asyncErrorBoundary(create)],
+    create: [
+        hasData, 
+        hasLatitude, 
+        hasLongitude, 
+        hasSkyCondition, 
+        asyncErrorBoundary(create)
+    ],
     list: asyncErrorBoundary(list),
     read: asyncErrorBoundary(read),
-    update: [hasData, hasLatitude, hasLongitude, observationExists, asyncErrorBoundary(update)]
+    update: [
+        hasData, 
+        hasLatitude, 
+        hasLongitude, 
+        hasAirTemperatureUnit, 
+        hasAirTemperature, 
+        observationExists, 
+        asyncErrorBoundary(update)
+    ]
 }
