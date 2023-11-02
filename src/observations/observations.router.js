7996a00c68db1baa171f5c5c9b46964addd66e85
James-Lycett <jrlycett@mail.com>
2023-11-02 08:46:59 -0700

    just added a comment

--- src/observations/observations.router.js
+++ src/observations/observations.router.js
@@ -2,6 +2,8 @@ const router = require("express").Router({ mergeParams: true })
 const controller = require("./observations.controller")
 const methodNotAllowed = require("../errors/methodNotAllowed")
 
+// CRUDL request handling for /observations routes
+
 router.route("/")
 .post(controller.create)
 .get(controller.list)
